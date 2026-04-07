95a744e11078d4090152168ccfabff78bd07eca8
FinkDev <pedro.fink06@gmail.com>
2026-04-07 00:51:29 -0300

    feat: adiciona estrutura global de estilos e layout base

---
+++ src/Views/RegisterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-container app-container-stripe">
+    <h1>Register</h1>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+    },
+  ],
 })
 
 export default router
