8584117d147f810b698483ba03a0ca16569843c9
Vinicius <vini.familalima@gmail.com>
2026-04-09 00:16:44 -0300

    Pagina de login

--- src/Views/LoginView.vue
+++ src/Views/LoginView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+    <section class="app">
+      <section class ="app-container app-container-stripe">
+        <form class ="input-area">
+        <section class ="title-card">
+          <h1 >LOGIN</h1>
+          <h2 >Insira suas credenciais para entrar no sistema</h2>
+        </section>
+        
+          <input class="input" type="email" name="email" placeholder="Email">
+          <input class="input" type="password" name="senha" placeholder="Senha">
+          <a href="*" class="link">Esqueceu a senha?</a>
+          <button class="btn">Entrar</button>
+        </form>
+        
+      </section>
+      <img src="./logo.png" alt="">
+    </section>
 </template>
 
 <style scoped></style>
